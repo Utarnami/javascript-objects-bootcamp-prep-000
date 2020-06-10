5253e9b78721588113ddda752ad52157baac63b3
Pavel Dziamentsyeu <paveldz@mail.com>
2020-06-10 21:16:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { 
-  PhilOchs: "Here's to the State of Mississippi"
+  "Phil Ochs": "Here's to the State of Mississippi"
 };
 
 function updatePlaylist (playlist, artistName, songTitle){
