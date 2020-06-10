4e1500b6bd560b397e65b974a22a705c0d48caac
Pavel Dziamentsyeu <paveldz@mail.com>
2020-06-10 21:07:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,9 @@
 var playlist = { 
   Slowdive: "Alison"
-  My Bloody Valentine: "Sometimes"
 };
 
 function updatePlaylist (playlist, artistName, songTitle){
+  playlist['My Bloody Valentine'] = 'Sometimes'
+  playlist['Slowdive'] = 'Alison'
   return playlist
 }
